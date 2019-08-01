fdc61bf5cda60590f005eeda14507562967f1902
Ingo <roessner@infai.org>
2019-08-01 15:32:17 +0200

    update id generation

--- lib/model/id.go
+++ lib/model/id.go
@@ -3,7 +3,7 @@ package model
 import "github.com/google/uuid"
 
 func (variable *Characteristic) GenerateId() {
-	variable.Id = "urn:infai:ses:categoryvariable:" + uuid.New().String()
+	variable.Id = URN_PREFIX + "characteristic:" + uuid.New().String()
 	for i, v := range variable.SubCharacteristics {
 		v.GenerateId()
 		variable.SubCharacteristics[i] = v
@@ -11,19 +11,26 @@ func (variable *Characteristic) GenerateId() {
 }
 
 func (class *DeviceClass) GenerateId() {
-	class.Id = "urn:infai:ses:device-class:" + uuid.New().String()
+	class.Id = URN_PREFIX + "device-class:" + uuid.New().String()
 }
 
 func (function *Function) GenerateId() {
-	function.Id = "urn:infai:ses:function:" + uuid.New().String()
+	switch function.RdfType {
+	case SES_ONTOLOGY_CONTROLLING_FUNCTION:
+		function.Id = URN_PREFIX + "controlling-function:" + uuid.New().String()
+	case SES_ONTOLOGY_MEASURING_FUNCTION:
+		function.Id = URN_PREFIX + "measuring-function:" + uuid.New().String()
+	default:
+		function.Id = ""
+	}
 }
 
 func (aspect *Aspect) GenerateId() {
-	aspect.Id = "urn:infai:ses:aspect:" + uuid.New().String()
+	aspect.Id = URN_PREFIX + "aspect:" + uuid.New().String()
 }
 
 func (category *Concept) GenerateId() {
-	category.Id = "urn:infai:ses:category:" + uuid.New().String()
+	category.Id = URN_PREFIX + "concept:" + uuid.New().String()
 	for i, v := range category.Characteristics {
 		v.GenerateId()
 		category.Characteristics[i] = v
@@ -31,11 +38,11 @@ func (category *Concept) GenerateId() {
 }
 
 func (device *Device) GenerateId() {
-	device.Id = "urn:infai:ses:device:" + uuid.New().String()
+	device.Id = URN_PREFIX + "device:" + uuid.New().String()
 }
 
 func (deviceType *DeviceType) GenerateId() {
-	deviceType.Id = "urn:infai:ses:device-type:" + uuid.New().String()
+	deviceType.Id = URN_PREFIX + "device-type:" + uuid.New().String()
 	for i, service := range deviceType.Services {
 		service.GenerateId()
 		deviceType.Services[i] = service
@@ -46,7 +53,7 @@ func (deviceType *DeviceType) GenerateId() {
 }
 
 func (service *Service) GenerateId() {
-	service.Id = "urn:infai:ses:service:" + uuid.New().String()
+	service.Id = URN_PREFIX + "service:" + uuid.New().String()
 	for i, function := range service.Functions {
 		if function.Id == "" {
 			function.GenerateId()
@@ -70,11 +77,11 @@ func (service *Service) GenerateId() {
 }
 
 func (hub *Hub) GenerateId() {
-	hub.Id = "urn:infai:ses:hub:" + uuid.New().String()
+	hub.Id = URN_PREFIX + "hub:" + uuid.New().String()
 }
 
 func (protocol *Protocol) GenerateId() {
-	protocol.Id = "urn:infai:ses:protocol:" + uuid.New().String()
+	protocol.Id = URN_PREFIX + "protocol:" + uuid.New().String()
 	for i, segment := range protocol.ProtocolSegments {
 		segment.GenerateId()
 		protocol.ProtocolSegments[i] = segment
@@ -82,16 +89,16 @@ func (protocol *Protocol) GenerateId() {
 }
 
 func (segment *ProtocolSegment) GenerateId() {
-	segment.Id = "urn:infai:ses:segment:" + uuid.New().String()
+	segment.Id = URN_PREFIX + "protocol-segment:" + uuid.New().String()
 }
 
 func (content *Content) GenerateId() {
-	content.Id = "urn:infai:ses:content:" + uuid.New().String()
+	content.Id = URN_PREFIX + "content:" + uuid.New().String()
 	content.ContentVariable.GenerateId()
 }
 
 func (variable *ContentVariable) GenerateId() {
-	variable.Id = "urn:infai:ses:contentvariable:" + uuid.New().String()
+	variable.Id = URN_PREFIX + "content-variable:" + uuid.New().String()
 	for i, v := range variable.SubContentVariables {
 		v.GenerateId()
 		variable.SubContentVariables[i] = v
