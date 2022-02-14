173000b88b26efffc26667c95072da6964a25f4d
IngoRoessner <roessner@infai.org>
2022-02-14 13:09:20 +0100

    remove RdfType field from DeviceType

--- lib/model/shared.go
+++ lib/model/shared.go
@@ -32,7 +32,6 @@ type DeviceType struct {
 	Services      []Service      `json:"services"`
 	DeviceClassId string         `json:"device_class_id"`
 	Attributes    []Attribute    `json:"attributes"`
-	RdfType       string         `json:"rdf_type"`
 }
 
 type ServiceGroup struct {
